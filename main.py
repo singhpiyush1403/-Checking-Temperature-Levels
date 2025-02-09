0292f6465faafa78715438303798da82bbfff9b8
PIYUSH SINGH <singhpiyush1403@gmail.com>
2025-02-09 14:04:14 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+temperature = int(input("Enter the temperature in Celsius: "))
+
+if temperature >= 35:
+    print("It's too hot! Stay hydrated.")
+elif temperature >= 25:
+    print("The weather is warm and pleasant.")
+elif temperature >= 15:
+    print("It's a bit cool. You might need a light jacket.")
+else:
+    print("It's cold! Wear warm clothes.")
